6f06df0a355f08bf6814ed5fd9b60b2fb45f6961
ridoyislamnasim <ridoyislamnasim@gmail.com>
2026-09-18 18:34:43 +0600

    local dev clear

---
+++ prisma/schema/migrations/20260918123425_rest_dev_lcal/migration.sql
@@ -0,0 +1,47 @@
+-- DropForeignKey
+ALTER TABLE "Attendance" DROP CONSTRAINT "Attendance_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "RolePermission" DROP CONSTRAINT "RolePermission_roleId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Shift" DROP CONSTRAINT "Shift_cashierId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "UserBranch" DROP CONSTRAINT "UserBranch_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "UserRole" DROP CONSTRAINT "UserRole_roleId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "UserRole" DROP CONSTRAINT "UserRole_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "UserTenant" DROP CONSTRAINT "UserTenant_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "UserTenant" ADD CONSTRAINT "UserTenant_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RolePermission" ADD CONSTRAINT "RolePermission_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserRole" ADD CONSTRAINT "UserRole_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserRole" ADD CONSTRAINT "UserRole_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserBranch" ADD CONSTRAINT "UserBranch_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Shift" ADD CONSTRAINT "Shift_cashierId_fkey" FOREIGN KEY ("cashierId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Attendance" ADD CONSTRAINT "Attendance_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
